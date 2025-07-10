4cf4a083dd59ca59f44719c4317204af5f7a9893
unknown <you@example.com>
2025-07-10 11:49:46 +0530

    toastify

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <BrowserRouter>
-      <ToastContainer position="top-center"
-        autoClose={3000} /> 
       <App />
+      <ToastContainer />
     </BrowserRouter>
   </Provider >
 )
